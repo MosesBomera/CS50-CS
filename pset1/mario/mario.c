83ffedad22898c902577c794e361997a18be8966
Moses Bomera <mosesbomer@gmail.com>
2020-05-24 16:22:46 +0300

    Implement Mario Easy

---
+++ pset1/mario/mario.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int n;
+
+    do
+    {
+       n = get_int("How high do you want the table? ");
+    }
+    while (n < 1 || n > 8);
+
+    for (int i = 1; i <= n; i++)
+    {
+        // The spaces
+        for (int k = 0; k < n - i; k++)
+        {
+            printf(" ");
+        }
+
+        // The '#'
+        for (int j = 1; j <= i; j++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    }
+    return 0;
+}
